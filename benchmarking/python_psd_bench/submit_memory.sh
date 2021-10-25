2a437716f8aaa24fabb2f5f002c5b4dfc616da3c
Forrest Koch <forrest.c.koch@gmail.com>
2021-10-26 10:26:28 +1100

    update the way things are imported

--- benchmarking/python_psd_bench/submit_memory.sh
+++
@@ -1,6 +0,0 @@
-for i in $(ls data/e18_samples/|cut -d'_' -f2|cut -d'-' -f1); do
-    for j in $(seq 1 4); do
-        qsub -o results/memory_profiler/memory_${i}_${j}.log -vSIZE=$i,REP=$j memory_psd.sh
-    done
-done
-
